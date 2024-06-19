abc1608c6275d0fdeef666a21bb72133ea6c7db6
Mehak Mattoo <77096365+Mehak-Mattoo@users.noreply.github.com>
2024-06-19 11:44:54 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// https://ibb.co/mHMxTKX
+// https://ibb.co/8NRtCRv
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import ClosedApplications from "./components/ClosedProject/ClosedApplication";
+// import Navbar from "./components/Navbar/CustomNavbar";
+// import UpcomingApplications from "./components/UpcomingProject/UpcomingProject";
+// import OpenApplications from "./components/OpenProjects/OpenApplication";
+// import Blogs from "./components/Blogs/Blogs";
+// import Login from "./components/Login";
+// import Signup from "./components/SignUp";
+
+// function App() {
+//   return (
+//     <Router>
+//       <>
+//         <Navbar />
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/closed-applications" element={<ClosedApplications />} />
+//           <Route path="/open-applications" element={<OpenApplications />} />
+//           <Route
+//             path="/upcoming-applications"
+//             element={<UpcomingApplications />}
+//           />
+//           <Route path="/blogs" element={<Blogs />} />
+//           <Route path="/log-in" element={<Login />} />
+//           <Route path="/sign-up" element={<Signup />} />
+//         </Routes>
+//       </>
+//     </Router>
+//   );
+// }
+
+// export default App;
